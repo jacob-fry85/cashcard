8d758130f198a98e73ab29970a19a726f1005d29
darwintirta <darwin.hermawan@gmail.com>
2026-04-03 14:51:17 +0700

    use spring data JDBC, h2 memory for integration test, generate init data using schema and data sqls, use Optional lambda .map .orElseGet

--- src/main/java/example/cashcard/controller/CashCardController.java
+++ src/main/java/example/cashcard/controller/CashCardController.java
@@ -1,23 +1,29 @@
 package example.cashcard.controller;
 
 import example.cashcard.model.CashCard;
+import example.cashcard.repository.CashCardRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return cashCardRepository.findById(requestedId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -22,6 +22,5 @@ class CashCardApplicationTests {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-		assertThat(response.getBody()).isBlank();
 	}
 }
