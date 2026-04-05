b8eef4de6e79dc0d3963a71970aad7b1cba70109
darwintirta <darwin.hermawan@gmail.com>
2026-04-05 14:58:11 +0700

    add spring boot starter security

---
+++ src/main/java/example/cashcard/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package example.cashcard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
